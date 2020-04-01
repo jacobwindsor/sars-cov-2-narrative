e290912cc856fa5aed7bc8c7ce492dd1eebdc7ee
Jacob Windsor <me@jcbwndsr.com>
2020-04-01 17:47:57 +0200

    Add initial template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Box, Grommet } from 'grommet';
+import React, { useState } from 'react';
+import { Box, Button, Collapsible, Grommet, Heading, Layer, ResponsiveContext } from 'grommet';
+import { FormClose, FormAdd } from 'grommet-icons'
 import theme from './theme'
 
 const AppBar = props => (
@@ -13,7 +14,9 @@ const AppBar = props => (
     elevation='medium'
     style={{ zIndex: '1' }}
     {...props}
-  />
+  >
+    <Heading level='3' margin='none'>{process.env.REACT_APP_APP_NAME}</Heading>
+  </Box>
 )
 
 const Narrative = props => (
@@ -25,26 +28,71 @@ const Narrative = props => (
   />
 )
 
-const Interaction = props => (
-  <Box
-    width='medium'
-    background='light-2'
-    elevation='small'
-    align='center'
-    justify='center'
-    {...props}
-  />
-)
+const Interaction = props => {
+  if (!props.show || props.size !== 'small') return (
+    <Collapsible direction='horizontal' open={props.show}>
+      <Box
+        flex
+        width='medium'
+        background='light-2'
+        elevation='small'
+        align='center'
+        justify='center'
+      >Interaction</Box>
+    </Collapsible>
+  )
 
-function App() {
   return (
-    <Grommet theme={theme}>
-      <AppBar>{process.env.REACT_APP_APP_NAME}</AppBar>
+    <Layer>
+      <Box
+        background='light-2'
+        tag='header'
+        justify='end'
+        align='center'
+        direction='row'
+      >
+        <Button
+          icon={props.show ? <FormClose /> : <FormAdd/> }
+          onClick={() => props.setShow(!props.show)}
+        />
+      </Box>
+      <Box
+       fill
+       background='light-2'
+       align='center'
+       justify='center'
+      >
+        Interaction
+      </Box>
+    </Layer>
+  )
+}
 
-      <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-        <Narrative>Narrative</Narrative>
-        <Interaction>Interaction</Interaction>
-      </Box>      
+
+const App = () => {
+  const [showInteraction, setShowInteraction] = useState(true)
+
+  return (
+    <Grommet theme={theme} full>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar size={size} />
+
+            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+              <Narrative>
+                <p>Narrative</p>
+                <Button
+                  onClick={() => setShowInteraction(!showInteraction)}
+                  label={`${showInteraction ? 'Hide' : 'Show'} interaction`}
+                  icon={showInteraction ? <FormClose/> : <FormAdd />}
+                />
+              </Narrative>
+              <Interaction show={showInteraction} setShow={setShowInteraction} size={size} />
+            </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>    
     </Grommet>
   );
 }
