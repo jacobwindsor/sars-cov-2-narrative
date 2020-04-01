a1c00df55665ec19bcb708dc0d7555e5569cea74
Jacob Windsor <me@jcbwndsr.com>
2020-04-01 16:48:32 +0200

    Add Grommet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grommet } from 'grommet';
+import theme from './theme'
+
+const AppBar = props => (
+  <Box 
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+)
+
+const Narrative = props => (
+  <Box
+    flex
+    align='center'
+    justify='center'
+    { ...props }
+  />
+)
+
+const Interaction = props => (
+  <Box
+    width='medium'
+    background='light-2'
+    elevation='small'
+    align='center'
+    justify='center'
+    {...props}
+  />
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme}>
+      <AppBar>{process.env.REACT_APP_APP_NAME}</AppBar>
+
+      <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+        <Narrative>Narrative</Narrative>
+        <Interaction>Interaction</Interaction>
+      </Box>      
+    </Grommet>
   );
 }
 
